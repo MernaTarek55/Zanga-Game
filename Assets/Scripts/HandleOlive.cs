c80506ef7ce312622198213c40086ab050a86d8e
MernaTarek55 <91115322+MernaTarek55@users.noreply.github.com>
2025-07-12 20:47:17 +0300

    fix olive movement sequence

--- Assets/Scripts/HandleOlive.cs
+++ Assets/Scripts/HandleOlive.cs
@@ -10,24 +10,24 @@ public class HandleOlive : MonoBehaviour
     {
         if (collision.CompareTag("hole"))
         {
-            Debug.Log("Hiiiiiii");
+            this.GetComponent<Collider2D>().enabled = false;
             Transform oliveTransform = this.transform;
             Transform playerTransform = Character.transform;
 
             Sequence oliveSequence = DOTween.Sequence();
 
 
-            oliveSequence.Append(oliveTransform.DOLocalMove(new Vector3(-1.26f, -2.25999999f, 0), 1f));
+            oliveSequence.Append(oliveTransform.DOLocalMove(new Vector3(oliveTransform.localPosition.x, oliveTransform.localPosition.y-3, 0), 1f));
 
-            oliveSequence.Append(playerTransform.DOMove(new Vector3(5f, -3f, 0f), 1.5f));
+            oliveSequence.Append(playerTransform.DOMove(new Vector3(playerTransform.position.x+10, playerTransform.position.y, 0f), 1.5f));
 
-            oliveSequence.Append(oliveTransform.DOLocalMove(new Vector3(-1.5f, -1.8f, 0f), 1f)
+            oliveSequence.Append(oliveTransform.DOLocalMove(new Vector3(oliveTransform.localPosition.x-0.2f, oliveTransform.localPosition.y-1.5f, 0f), 1f)
                 .SetEase(Ease.OutBack));
 
             oliveSequence.OnComplete(() => {
                 if (CharacterHead != null)
                 {
-                    CharacterHead.transform.rotation = Quaternion.Euler(0f, 0f, 16.083f);
+                    CharacterHead.transform.rotation = Quaternion.Euler(0f, 0f, CharacterHead.transform.rotation.z+16f);
                 }
             });
         }
